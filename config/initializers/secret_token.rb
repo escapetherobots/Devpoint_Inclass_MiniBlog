c731186889674fd88d6efee2850ffff60039caa2
TheNinJedi <theninjedi@gmail.com>
2013-09-27 16:09:34 -0600

    Starting the Rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniBlog::Application.config.secret_token = 'a54b06a28720982967300930439b0483f504a8ae8ba44ef6c19b9f5d40b03c6310eedfdffdbdcf114aebb173b5a1adbf8066f6216b51f33971e8dda012d75756'
