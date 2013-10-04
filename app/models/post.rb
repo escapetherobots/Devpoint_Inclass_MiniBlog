1d734069e40f15ca199ffb5c95813162f53acc5a
Ryan Holdaway <rholdy@gmail.com>
2013-10-03 19:19:52 -0600

    Adds new comment scaffold with associations and displays on post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :author
+  has_many :comments
   # hey these posts now belong to an author!
 
   attr_accessible :body, :published_at, :title, :author_id
